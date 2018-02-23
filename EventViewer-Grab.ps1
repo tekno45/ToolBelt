cbc7656303093386513d0f1f52aaf1cb6808aaf2
tekno45 <megamanX42002@gmail.com>
2018-02-23 15:07:19 -0800

    Find-VM Add
    Find-VM: Command to find VM's across SCVMM environment
    
    EventViewer-Grab: Grab remote Event logs for set time periods

---
+++ EventViewer-Grab.ps1
@@ -0,0 +1,23 @@
+﻿
+function GetEventsNShit {
+
+        $chromeCache='%LocalAppData%\Google\Chrome\User Data\Default\Cache'
+        $localhost = 'localhost'
+
+param( [string]$computerName = $localhost ,[datetime]$startTime=(Get-Date).AddMinutes(-15) ,[datetime]$Endtime=(Get-DAte))
+
+$logs = (Get-WinEvent -ListLog * -ComputerName $computerName | Where-Object{$_.recordCount}).Logname
+
+$filterTable=@{
+        'StartTime' = $startTime
+        'EndTime' = $Endtime
+        'LogName' = $logs
+}
+
+$events = Get-WinEvent -ComputerName $computerName -FilterHashtable $filterTable -ErrorAction SilentlyContinue
+
+Return $events
+
+
+}
+
---
+++ Find-VM.ps1
@@ -0,0 +1,24 @@
+﻿function Find-VM{
+
+param
+([string]$ComputerName)
+
+$servers = "SCVMMCSS2", "OAKITVMM", "OAKENGSCVMM2"
+
+foreach($server in $servers){   
+
+$scvmmserv = Get-SCVMMServer $server
+$VM = Get-SCVirtualMachine $ComputerName
+
+
+"i can string wherever i want"
+
+if($VM.Name){Return $VM, $scvmmserv} 
+
+
+Else{"No VM"}
+
+}
+
+
+}
