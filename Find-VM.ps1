4ccc813f301354a1e74e013f84200284b9606f11
tekno45 <megamanX42002@gmail.com>
2018-02-27 10:44:34 -0800

    Removed hard coded servers
    Now servers are imported from a CSV file. Small changes to pull proper strings in SCVMM server calls

--- Find-VM.ps1
+++ Find-VM.ps1
@@ -1,20 +1,21 @@
 ﻿function Find-VM{
 
 param
-([string]$ComputerName)
+([string]$ComputerName,
+$credentials)
 
 #List of SCVMM servers to query
-$servers = "SCVMMCSS2", "OAKITVMM", "OAKENGSCVMM2"
+$servers = Import-Csv -Path C:\users\jgreene\Documents\ToolBelt\SCVMM_Servers.csv
 
 #Loop through servers and search for machine on each
 foreach($server in $servers){   
 
 #Capture server and VM info
-$scvmmserv = Get-SCVMMServer $server
-$VM = Get-SCVirtualMachine $ComputerName
+$scvmmserv = Get-SCVMMServer $server.Server -Credential $credentials
+$VM = Get-SCVirtualMachine -All -VMMServer $scvmmserv | Where-Object{$_.ComputerName -eq "$ComputerName.osisoft.int" -or $_.ComputerName -eq "$computerName.dev.osisoft.int"}
 
 #Once found return info
-if($VM.Name){Return $VM, $scvmmserv} 
+if($VM.Name){Return $VM} 
 
 }
 #NO VM FOUND 
