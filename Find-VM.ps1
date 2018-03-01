5bef5847f38e847d425f4fd054909b40e752dbe6
tekno45 <megamanX42002@gmail.com>
2018-03-01 15:58:38 -0800

    more cleaning.  Added a quick message Might add full VM stats as verbose

--- Find-VM.ps1
+++ Find-VM.ps1
@@ -3,22 +3,31 @@
 param
 (
 [parameter(Mandatory=$true)][string]$ComputerName,
-[securestring]$credentials,
-[parameter]$servers = (Import-Csv -Path C:\users\jgreene\Documents\ToolBelt\SCVMM_Servers.csv)
+[pscredential]$credentials,
+$servers = (Import-Csv -Path C:\users\jgreene\Documents\ToolBelt\SCVMM_Servers.csv)
 )
 
-#List of SCVMM servers to query
-$servers = Import-Csv -Path C:\users\jgreene\Documents\ToolBelt\SCVMM_Servers.csv
+
 
 #Loop through servers and search for machine on each
 foreach($server in $servers){   
 
+
 #Capture server and VM info
 $scvmmserv = Get-SCVMMServer $server.Server -Credential $credentials
-$VM = Get-SCVirtualMachine -All -VMMServer $scvmmserv | Where-Object{$_.ComputerName -eq "$ComputerName.osisoft.int" -or $_.ComputerName -eq "$computerName.dev.osisoft.int"}
+$VM = Get-SCVirtualMachine -All -VMMServer $scvmmserv | `
+Where-Object{$_.ComputerName -eq "$ComputerName.osisoft.int" -or $_.ComputerName -eq "$computerName.dev.osisoft.int"}
 
 #Once found return info
-if($VM.Name){Return $VM} 
+
+if($VM){
+    $status = $vm.VirtualMachineState
+    $environemnt = $scvmmserv.Name
+   Write-Host "$computerName found in $environemnt environment `n"
+
+    "It is currently $status"
+    
+    Return $VM } 
 
 }
 #NO VM FOUND 
