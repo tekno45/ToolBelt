03e4da1fc98e1dd98b2f0de001dac27278e556fe
tekno45 <megamanX42002@gmail.com>
2018-03-06 09:20:36 -0800

    Cleaning up i guess

--- Find-VM.ps1
+++ Find-VM.ps1
@@ -7,26 +7,23 @@ param
 $servers = (Import-Csv -Path C:\users\jgreene\Documents\ToolBelt\SCVMM_Servers.csv)
 )
 
-
-
 #Loop through servers and search for machine on each
 foreach($server in $servers){   
 
-
+    
 #Capture server and VM info
-$scvmmserv = Get-SCVMMServer $server.Server -Credential $credentials
-$VM = Get-SCVirtualMachine -All -VMMServer $scvmmserv | `
-Where-Object{$_.ComputerName -eq "$ComputerName.osisoft.int" -or $_.ComputerName -eq "$computerName.dev.osisoft.int"}
+    $scvmmserv = Get-SCVMMServer $server.Server -Credential $credentials
+    $VM = Get-SCVirtualMachine -All -VMMServer $scvmmserv | `
+    Where-Object{$_.ComputerName -eq "$ComputerName.osisoft.int" -or $_.ComputerName -eq "$computerName.dev.osisoft.int"}
 
 #Once found return info
 
 if($VM){
     $status = $vm.VirtualMachineState
     $environemnt = $scvmmserv.Name
-   Write-Host "$computerName found in $environemnt environment `n"
 
-    "It is currently $status"
-    
+#Quick Statement
+    Write-Host "$computerName found in $environemnt environment `n" + "It is currently $status"
     Return $VM } 
 
 }
